8dbf7942e3fd2e8d3ba6ef56eb3ef4d1955f1da6
Brandon J. Fantine <brandonfantine@gmail.com>
2024-07-08 11:22:33 -0500

    Added DOM loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,14 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-console.log("Page is loading, please wait...")
-
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('loadpage.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('load').innerHTML = data;
+            console.log("Content loaded successfully.");
+        })
+        .catch(error => {
+            console.error("Error loading content:", error);
+        });
+});
